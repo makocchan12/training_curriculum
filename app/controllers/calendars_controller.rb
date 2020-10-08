3b84142a2adf42617ba4ecc317aabdc7fcefcbe8
makocchan12 <shootingstar1119@gmail.com>
2020-10-08 16:42:18 +0900

    Issue6

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,5 +1,5 @@
 class CalendarsController < ApplicationController
-
+  require "date"
   # １週間のカレンダーと予定が表示されるページ
   def index
     getWeek
@@ -34,9 +34,14 @@ class CalendarsController < ApplicationController
       plan = plans.map do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
+      
+      wday_num = Date.today.wday#Date.today.wdayを利用して添字となる数値を得る
+      #もしもwday_numが7以上であれば、7を引く
+      if  wday_num >= 7
+        wday_num = wday_num - 7
+      end
+      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans, wday: wdays[(@todays_date + x).wday]}
       @week_days.push(days)
     end
-
   end
 end
